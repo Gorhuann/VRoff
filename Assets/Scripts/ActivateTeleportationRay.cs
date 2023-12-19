858cdf55f477c2585a7e468b1598d8b341230ffa
AlexandreQuesnay <quesnay.alexandre@gmail.com>
2023-12-19 17:55:00 +0100

    Partie 2 du commit de fou

--- Assets/Scripts/ActivateTeleportationRay.cs
+++ Assets/Scripts/ActivateTeleportationRay.cs
@@ -1,21 +1,3 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-using UnityEngine.XR.Interaction.Toolkit;
-using UnityEngine.InputSystem;
-
-public class ActivateTeleportationRay : MonoBehaviour
-{
-
-    public GameObject rightTeleportation;
-
-    public InputActionProperty rightActivate;
-
-    // Update is called once per frame
-    void Update()
-    {
-        float value = rightActivate.action.ReadValue<float>();
-        Debug.Log(value);
-        rightTeleportation.SetActive(rightActivate.action.ReadValue<float>() > 0.1f);
-    }
-}
+version https://git-lfs.github.com/spec/v1
+oid sha256:e1bcb6d73d43966d626957bd6bb9592c10274f9d2c27d90e9eba80f7cd87019a
+size 671
